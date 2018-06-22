2b214ee97488123f4f9919859808f0f27241fa90
Anan Roderick <thamarnan@gmail.com>
2018-06-22 16:11:44 -0700

    refresh templates.json for every new custom png

---
+++ refresh_template.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+filename='./templates.json'
+
+#md5sum of templates.json version 1.4.0.7
+original_md5=0e7d0b7175e8d99028e0b66756f27f08
+original_file="templates.json.original"
+current_file="templates.json"
+
+if [ `md5sum  templates.json | awk {'print $1'}` = $original_md5 ]; then
+    echo "[INFO] found factory codex 1.4.0.7 templates.json"
+    echo "[INFO] Backing up to templates.json.original"
+    cp templates.json templates.json.original
+else
+#    echo "[INFO] Modified version temlates.json.. backing with date"
+    today=`date +%Y-%m-%d-%H_%M_%S`
+#    cp templates.json templates.json_$today
+fi
+
+lastlinen=`expr $(wc -l templates.json.original | awk '{print $1}') - 2`
+sed -n "1,$lastlinen p" templates.json.original > templates.json
+
+for templatefile in *.png ; do
+
+#    echo $templatefile
+    name="${templatefile%.*}"
+    ext="${templatefile##*.}"
+
+    #Check if this is in factory list
+    grep -w "$name" $original_file > /dev/null
+    if [ $? -eq '0' ]
+    then
+    A="original"
+    #echo "[INFO] $templatefile is in $original_file skipped"
+ else
+   echo "[INFO] $templatefile is new."
+    # Appending custom list
+
+    printf "\n\t,{">> $current_file
+    printf "\t\t\"name\": \"%s\",\n" "$name"  >> $current_file
+    printf "\t\t\"filename\": \"%s\",\n" "$name"  >> $current_file
+    printf "\t\t\"iconCode\": \"%s\",\n" "\\ue9fe"  >> $current_file
+
+    #Adding ladscape true of LS Found
+    if [[ $name = *"LS"* ]]; then
+        printf "\t\t\"landscape\": \"%s\",\n" "true"  >> $current_file
+    fi
+
+    #Default to Creative Category
+    printf "\t\t\"categories\": [\n"  >> $current_file
+        printf "\t\t\t\"Creative\"\n"  >> $current_file
+    printf "\t\t]\n"  >> $current_file
+    printf "\t}"  >> $current_file
+    
+    fi
+done
+
+printf "\n\t]\n"  >> $current_file
+printf "}\t\n"  >> $current_file
